3908a0c6065fc181f7be18b1984dbeb0e62660fc
matthewvvirtue <matthewvvirtue@gmail.com>
2026-04-14 16:38:54 +0000

    Added to the PlannerManger class and set up the the intial plans and changes to the main output structures

--- src/HangoutApp.java
+++ src/HangoutApp.java
@@ -2,23 +2,24 @@ import java.util.Scanner;
 
 public class HangoutApp {
     public static void main(String[] args) {
+        PlannerManager manager = new PlannerManager();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter your budget: ");
+        System.out.println("--- SD Hangout Planner ---");
+
+        System.out.print("Enter your max budget: ");
         double budget = scanner.nextDouble();
 
         System.out.print("Enter the number of friends: ");
         int numFriends = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.print("Enter your preferences (e.g., outdoor, indoor, food): ");
-        String preferences = scanner.nextLine();
+        System.out.print("Enter current weather (Sunny/Rainy): ");
+        String weather = scanner.nextLine();
 
-        System.out.println("Budget: " + budget);
-        System.out.println("Number of friends: " + numFriends);
-        System.out.println("Preferences: " + preferences);
+        System.out.println("Checking " + manager.getAllActivities().size() + " total activities...");
 
-        //Generate random plan based on inputs
+
 
         scanner.close();
     }
--- src/PlannerManager.java
+++ src/PlannerManager.java
@@ -1,5 +1,25 @@
 import java.util.ArrayList;
 
 public class PlannerManager {
+    private ArrayList<Activity> allActivities;
 
+    public PlannerManager() {
+        allActivities = new ArrayList<>();
+        loadStarterActivities();
+    }
+
+    private void loadStarterActivities() {
+        allActivities.add(new Activity("Sunset Cliffs Hike", 0.0, 1, 10, "Sunny", "Outdoor"));
+        allActivities.add(new Activity("Belmont Park Rollercoaster", 15.0, 2, 6, "Sunny", "Adventurous"));
+        allActivities.add(new Activity("Liberty Public Market", 25.0, 1, 8, "Rainy", "Food"));
+        allActivities.add(new Activity("Coronado Ferry Ride", 7.0, 1, 20, "Sunny", "Chill"));
+    }
+
+    public void addActivity(Activity a) {
+        allActivities.add(a);
+    }
+
+    public ArrayList<Activity> getAllActivities() {
+        return allActivities;
+    }
 }
